46d0b113d1778c9479e6628d0a0d614620817bfc
Kalunnenna1 <nnennakalualie.com>
2022-05-21 05:05:47 -0700

    our commit for printf task

---
+++ printshort_hex.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * printshort_hex - prints a short decimal in hexadecimal
+ * @arguments: input string
+ * @buffer: buffer pointer
+ * @index_buffer: index for buffer pointer
+ * Return: number of chars printed
+ */
+int printshort_hex(va_list arguments, char *buffer, unsigned int index_buffer)
+{
+	short int int_input, i, isnegative, count, first_digit;
+	char *hexadecimal, *binary;
+
+	int_input = va_arg(arguments, int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		index_buffer = handle_buffer(buffer, '0', index_buffer);
+		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+
+	binary = malloc(sizeof(char) * (16 + 1));
+	binary = build_binary_array(binary, int_input, isnegative, 16);
+	hexadecimal = malloc(sizeof(char) * (4 + 1));
+	hexadecimal = build_hex_array(binary, hexadecimal, 0, 4);
+	for (first_digit = i = count = 0; hexadecimal[i]; i++)
+	{
+		if (hexadecimal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			index_buffer = handle_buffer(buffer, hexadecimal[i], index_buffer);
+			count++;
+		}
+	}
+	free(binary);
+	free(hexadecimal);
+	return (count);
+}
+
