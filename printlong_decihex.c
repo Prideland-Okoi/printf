95edd31b7ad552062687c9a28a2b77fa9b1f1145
Kalunnenna1 <nnennakalualie.com>
2022-05-21 04:12:49 -0700

    our commit for printf task

---
+++ printlong_decihex.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * printlong_decihex - prints a long decimal in hexadecimal
+ * @arguments: input string
+ * @buffer: buffer pointer
+ * @index_buffer: index for buffer pointer
+ * Return: number of chars printed
+ */
+int printlong_decihex(va_list arguments, char *buffer, unsigned int index_buffer)
+{
+	long int int_input, i, isnegative, count, first_digit;
+	char *hexadecimal, *binary;
+
+	int_input = va_arg(arguments, long int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		index_buffer = handle_buffer(buffer, '0', index_buffer);
+		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+
+	binary = malloc(sizeof(char) * (64 + 1));
+	binary = build_binary_array(binary, int_input, isnegative, 64);
+	hexadecimal = malloc(sizeof(char) * (16 + 1));
+	hexadecimal = build_hex_array(binary, hexadecimal, 0, 16);
+	for (first_digit = i = count = 0; hexadecimal[i]; i++)
+	{
+		if (hexadecimal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			index_buffer = handle_buffer(buffer, hexadecimal[i], index_buffer);
+			count++;
+		}
+	}
+	free(binary);
+	free(hexadecimal);
+	return (count);
+}
+
